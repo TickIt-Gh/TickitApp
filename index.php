895ebf64dbcaa4d6224c87e5abe770c2c5f66fb7
razakadam74 <razakadam74@gmail.com>
2017-02-25 13:23:35 +0000

    updated index.php logo link updated

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
 <body>
 	<nav class="navbar navbar-default navbar-fixed-top" role='navigation'>
 		<div class="container">
-			<div class="navbar-header"><a href="#" class="navbar-brand navbar-link"><i class="glyphicon glyphicon-phone"></i>TickIT</a>
+			<div class="navbar-header"><a href="index.php" class="navbar-brand navbar-link"><i class="glyphicon glyphicon-phone"></i>TickIT</a>
 				<button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggle collapsed"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
 			</div>
 			<div class="collapse navbar-collapse" id="navcol-1">

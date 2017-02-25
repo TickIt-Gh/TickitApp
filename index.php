a0acfb2f91685c4b0f6aa83a299c6723af7c2f26
razakadam74 <razakadam74@gmail.com>
2017-02-25 14:37:03 +0000

    Sign out page updated

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>TickIT</title>
+	<title>Welcome to TickIT</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<style type="text/css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!--style type="text/css">
 		
 		p{
 			font-style: justify;
@@ -18,7 +19,7 @@
 			color: white;
 			background-size: cover;
 		}
-	</style>
+	</style-->
 </head>
 <body>
 	<nav class="navbar navbar-default navbar-fixed-top" role='navigation'>
@@ -103,7 +104,7 @@
 			&nbsp
 		</div>
 
-		<footer style="text-align: center;">
+		<footer id="footer" style="text-align: center;">
 			<div class="container">
 				<div class="custom"  >
 					<p>©TickIT Company Ltd. All rights reserved.</p>
---
+++ sign_up.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sign Up | TickIT</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top" role='navigation'>
+		<div class="container">
+			<div class="navbar-header"><a href="index.php" class="navbar-brand navbar-link"><i class="glyphicon glyphicon-phone"></i>TickIT</a>
+				<button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggle collapsed"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+			</div>
+			<div class="collapse navbar-collapse" id="navcol-1">
+				<ul class="nav navbar-nav navbar-left">
+					<li role="presentation"><a href="index.php">Home</a></li>
+					<li role="presentation"><a href="services.php">Service</a></li>
+					<li role="presentation"><a href="team.php">Team</a></li>
+					<li role="presentation"><a href="about.php">About Us</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+				</ul>
+			</div>
+
+		</div>
+	</nav>
+
+
+
+	<div class="row" style="margin:50px; margin-bottom: 100px;">
+		<div class="col-md-4">
+			&nbsp
+		</div>
+		<div class="col-md-4" ">
+			<form class="form-signin" method="POST" action="#">
+				<h2 class="form-signin-heading">Please sign in</h2>
+				<div class="input-group">
+					<span class="input-group-addon"><i class="fa fa-envelope-o fa-fw"></i></span>
+					<input type="username" id="inputEmail" class="form-control" placeholder="Username" required autofocus
+					name="username">
+				</div>
+				<div class="input-group">
+					<span class="input-group-addon"><i class="fa fa-key fa-fw"></i></span>
+					<input class="form-control" type="password" placeholder="Password" required name="password">
+				</div>
+				<div class="checkbox">
+					<label>
+						<input type="checkbox" value="remember_me"> Remember me
+					</label>
+				</div>
+				<div class="error">
+					<!--
+						PHP CODE FOR ERROR HERE
+					-->	
+				</div>
+				<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+			</form>
+		</div>
+		<div class="col-md-4">
+			&nbsp
+		</div>
+
+	</div>
+
+
+
+
+	<footer style="text-align: center;" id="footer">
+		<div class="container">
+			<div class="custom"  >
+				<p>©TickIT Company Ltd. All rights reserved.</p>
+				<p>1 University Avenue, PMB CT3321 | Cantonments, Accra, Ghana | Phone: +233.000.000.00</p>
+				<p><a href="index.php" rel="alternate">TickIT Company Ltd</a> | <a href="book_ticket" rel="alternate">Ticket Booking</a></p>
+			</div>
+		</div>
+
+	</footer>
+
+
+</body>
+</html>

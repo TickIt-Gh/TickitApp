2c0c42b7f27df0cca398e8516d1d3c28598744c1
Bhebhe <linda.bhebhe@ashesi.edu.gh>
2017-02-25 17:40:43 +0100

    Sign in and Out optimized

--- login.php
+++ login.php
@@ -38,16 +38,19 @@
 		</div>
 		<div class="col-md-4" ">
 			<form class="form-signin" method="POST" action="#">
-				<h2 class="form-signin-heading">Please sign in</h2>
+				<h2 class="form-signin-heading" style="text-align: center;">Please sign in</h2>
+				<br>
 				<div class="input-group">
 					<span class="input-group-addon"><i class="fa fa-envelope-o fa-fw"></i></span>
 					<input type="username" id="inputEmail" class="form-control" placeholder="Username" required autofocus
 					name="username">
 				</div>
+				<br>
 				<div class="input-group">
 					<span class="input-group-addon"><i class="fa fa-key fa-fw"></i></span>
 					<input class="form-control" type="password" placeholder="Password" required name="password">
 				</div>
+				<br>
 				<div class="checkbox">
 					<label>
 						<input type="checkbox" value="remember_me"> Remember me
@@ -56,6 +59,7 @@
 					<a href= "">forgot password?</a>
 					</label>
 				</div>
+				<br>
 				<div class="error">
 					<!--
 						PHP CODE FOR ERROR HERE

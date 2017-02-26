49a7fc1d22d2f39e321cd8074a051d5da73bdb63
Job mwesigwa <Job mwesigwa>
2017-02-26 22:34:35 +0000

    Changes in buy page

--- buy.php
+++ buy.php
@@ -3,6 +3,8 @@
 <head>
 	<title>Buy | TickIT</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Required Scripts and links for designing-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -10,34 +12,46 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+
+	<!--Navigation bar-->
 	<nav class="navbar navbar-default navbar-fixed-top" role='navigation'>
 		<div class="container">
+
+			<!--Navigation bar -->
 			<div class="navbar-header"><a href="index.php" class="navbar-brand navbar-link"><i class="glyphicon glyphicon-phone"></i>TickIT</a>
 				<button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggle collapsed"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
 			</div>
+
+			<!--Menues on the left side-->
 			<div class="collapse navbar-collapse" id="navcol-1">
+
 				<ul class="nav navbar-nav navbar-left">
 					<li role="presentation"><a href="index.php">Home</a></li>
-					<li role="presentation"><a href="buy.php">Buy</a></li>
+					<li role="presentation"><a href="itinerary.php">Bus Listing</a></li>
 					<li role="presentation"><a href="contact.php">Contact Us</a></li>
 					<li role="presentation"><a href="team.php">Team</a></li>
-					<li role="presentation"><a href="about.php">About Us</a></li>
 				</ul>
+
+				<!--Menues on the left side-->
 				<ul class="nav navbar-nav navbar-right">
 					<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
 					<li><a href="sign_up.php"><span class="fa fa-user-plus"></span> Sign Up</a></li>
 				</ul>
+
 			</div>
 
 		</div>
 	</nav>
 
 
-
 	<div class="row" style="margin:50px; margin-bottom: 100px;">
+
+		<!--Creating space on the left of the page content-->
 		<div class="col-md-4">
 			&nbsp
 		</div>
+
+		<!--Getting User Information-->
 		<div class="col-md-6" ">
 			<form class="form-signin" method="POST" action="#">
 
@@ -52,23 +66,25 @@
 				</div><br>
 
 				<div>
-					<input type="text" id="num" class="form-control" placeholder="Your name" required autofocus name="name">
+					<input type="text" id="tel" class="form-control" placeholder="Tel- (+233503548654)" required autofocus name="tel">
 				</div><br>
 
 				<!-- Large modal -->
 				<button type="button" class="btn btn-primary center-block btn-lg btn-block" onclick="validate_contact_form()" data-toggle="modal" data-target=".bs-example-modal-lg" >Buy
 				</button>
 
-				<script type ="text/javascript" src="buyscript.js"></script>
+				<!--Dive to have the modal information-->
 				<div id="pop">
 					
 				</div>
 
 				<!--including the javascript-->
-	<script type="text/javascript" src="js/script.js"></script>	
+				<script type="text/javascript" src="js/script.js"></script>	
 
 			</form>
 		</div>
+
+		<!--Creating empty space at the page content-->
 		<div class="col-md-4">
 			&nbsp
 		</div>
@@ -76,7 +92,7 @@
 	</div>
 
 
-
+		<!--The Footer of every page-->
        <div style=" padding-top:1% ">
 	<footer style="text-align: center; padding-top: 1%" id="footer">
 		<div class="container">
@@ -90,8 +106,6 @@
 	</footer>
 	</div>
 
-	<!--including the javascript-->
-	<script type="text/javascript" src="js/script.js"></script>
 
 </body>
 </html>

0286518671f1a87487eb3f4b5ede000b3b00ac3b
razakadam74 <razakadam74@gmail.com>
2017-02-26 10:49:44 +0000

    Index and Buy Nav bar edited

--- contact.php
+++ contact.php
@@ -2,16 +2,12 @@
 <html>
 <!--@uthor Brenda Mboya.This is the code for the contact page our the TICKIT application-->
 <head>
-	<title>Contact Us</title>
+	<title>Contact US | TickIT</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<link rel="stylesheet" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	
-	<link rel="stylesheet" href="css/bootstrap.min.css">
-<script href="js/bootstrap.min.js"></script>
 
 </head>
 <body>
@@ -22,45 +18,47 @@
 			</div>
 			<div class="collapse navbar-collapse" id="navcol-1">
 				<ul class="nav navbar-nav navbar-left">
-					<li role="presentation"><a href="index.php">Home</a></li>
+					<li role="presentation" ><a href="index.php">Home</a></li>
 					<li role="presentation"><a href="buy.php">Buy</a></li>
+					<li role="presentation" class="active" ><a href="contact.php">Contact Us</a></li>
 					<li role="presentation"><a href="team.php">Team</a></li>
 					<li role="presentation"><a href="about.php">About Us</a></li>
 				</ul>
 				<ul class="nav navbar-nav navbar-right">
 					<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+					<li><a href="sign_up.php"><span class="glyphicon glyphicon-log-in"></span> Sign Up</a></li>
 				</ul>
 			</div>
 
 		</div>
 	</nav>
 
-	
 
-<div class="row" style="margin:50px; margin-bottom: 100px;">
+
+	<div class="row" style="margin:50px; margin-bottom: 100px;">
 		<div class="col-md-4">
 			&nbsp
 		</div>
-<div class="col-md-6" >
-	<form class="form-signin" method="post" action="#" >
+		<div class="col-md-6" >
+			<form class="form-signin" method="post" action="#" >
 
-		 <input type="text" id="name" class="form-control" placeholder="Your name" required autofocus name="firstname">
-							 <br><br>
+				<input type="text" id="name" class="form-control" placeholder="Your name" required autofocus name="firstname">
+				<br>
 
-		<input type="username" class="form-control" id= "email" size="50" placeholder="your email" required autofocus>
-		 <br><br>
+				<input type="username" class="form-control" id= "email" size="50" placeholder="your email" required autofocus>
+				<br>
 
-		<textarea  rows="10" class="form-control" cols="50" id="comment" placeholder="your message here"></textarea>
-		<br><br>
+				<textarea  rows="10" class="form-control" cols="50" id="comment" placeholder="your message here"></textarea>
+				<br>
 
-		<input type="submit" class="btn btn-primary btn-block" name="submit" value="SUBMIT" onclick="validate()" >
+				<input type="submit" class="btn btn-primary btn-block" name="submit" value="SUBMIT" onclick="validate()" >
 
-	</form>
-	</div>
+			</form>
+		</div>
 	</div>
 
 	<script type="text/javascript" src="contact.js"></script>
-	<footer style="text-align: center; padding-top: 50%" id="footer">
+	<footer style="text-align: center; /*padding-top: 50%*/" id="footer">
 		<div class="container">
 			<div class="custom"  >
 				<p>©TickIT Company Ltd. All rights reserved.</p>
@@ -71,7 +69,7 @@
 
 	</footer>
 	<!--including the javascript-->
-	<script type="text/javascript" src="contact.js"></script>
+	<script type="text/javascript" src="js/contact.js"></script>
 </body>
-	
+
 </html>

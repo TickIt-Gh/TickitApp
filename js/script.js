7999992bbd317583b9aaaba056bcbe9c90d2220d
Bhebhe <linda.bhebhe@ashesi.edu.gh>
2017-02-26 21:44:27 +0100

    Login and Sign up mantaining form status

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
+
+//function to validate the email address
 function validate_email(email) {
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email.value);
 }
 
+//function to validate the password 
 function validate_password(password) {
 	var re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,16}$/;
 	return re.test(password.value);
 }
 
 
+//function to validate the name 
 function validate_name(name){
 	return /^[A-Za-z\s-]+$/.test(name.value) || /^[a-zA-Z ]+$/.test( name.value);
 
 }
 
-
+//function to validate the form for log in 
 function validate_login(){
 	var email = document.getElementById('email');
 	var password = document.getElementById('password');
 	if (email != null && password != null){
 		if (validate_email(email)){
-			if (validate_password(password))
+			
 				alert('Successful Login in');
-			else
-				alert('Password must be aleast 8 character \n one symbol \natleast an upper case \na lower case \na number');
+			
 		}else{
 			alert('Please a valid email');
 		}
@@ -34,6 +37,8 @@ function validate_login(){
 
 }
 
+
+//function to validate the form for sign up
 function validate_sign_up(){
 	var last_name = document.getElementById('last_name');
 	var first_name = document.getElementById('first_name');
@@ -42,30 +47,16 @@ function validate_sign_up(){
 	var email = document.getElementById('email');
 	var gender = document.getElementById('gender');
 
-	//alert(gender.value);
 
-	/*
-	if (date_of_birth.value == ""){
-		alert('No date selected');
-	}else
-		alert(date_of_birth.value);
-	alert( last_name.value +'<br>'+
-		first_name.value + '<br>'+
-		date_of_birth.value + '<br>'+
-		password.value +'<br>'+
-		email.value + '<br>'+
-		male.value + '<br>'+
-		female.value + '<br>'+
-		'<br>');
-		
-		*/
 		if (validate_name(last_name)){
 			if (validate_name(first_name)){
 				if (date_of_birth.value != ""){
 					if(validate_password(password)){
 						if (validate_email(email)){
 							if (gender.value != 'gender'){
-								alert('working');
+
+								//if all the details are valid, the user account is created 
+								alert('You have created an account with TickIt');
 							}else
 							alert('Select your gender');
 						}else
@@ -83,6 +74,7 @@ function validate_sign_up(){
 	}
 
 
+    //function to validate the contact form 
 	function validate_contact_form(){
 		var nam = document.getElementById("name");
 		var mail = document.getElementById("email");
@@ -91,11 +83,14 @@ function validate_sign_up(){
 		if (validate_name(nam)){
 			if (validate_email(mail)){
 				if (message.value != ""){
+
+					//if all the details are valid, the details are sent
+					//notify the user that the details have been sent
 					alert("your message has been sent to the tickit it team.We will respond as soon as possible");	
 				}else
 				alert('Enter your message');
 			}else
-			alert('Provide a valid email');
+			alert('Provide a value email');
 		}else
 		alert('Provide a valid name');
 

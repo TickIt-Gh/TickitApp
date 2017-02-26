a21fac8f72a4197a4903a45edbc98eb0825ee650
brenda.mboya <brendamboya.mboya@gmail.com>
2017-02-26 23:03:32 +0300

    added roles to the team page

--- team.php
+++ team.php
@@ -1,126 +1,123 @@
-			<html>
-			<head>
-				<title>teamPage</title>
-				<meta name="viewport" content="width=device-width, initial-scale=1">
-				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-				<link rel="stylesheet" type="text/css" href="css/style.css">
-				<link rel="stylesheet" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-			</head>
-			<body>
-			<nav class="navbar navbar-default navbar-fixed-top" role='navigation'>
+				<html>
+				<head>
+					<title>teamPage</title>
+					<meta name="viewport" content="width=device-width, initial-scale=1">
+					<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+					<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+					<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+					<link rel="stylesheet" type="text/css" href="css/style.css">
+					<link rel="stylesheet" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+				</head>
+				<body>
+				<nav class="navbar navbar-default navbar-fixed-top" role='navigation'>
+						<div class="container">
+							<div class="navbar-header"><a href="index.php" class="navbar-brand navbar-link"><i class="glyphicon glyphicon-phone"></i>TickIT</a>
+								<button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggle collapsed"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+							</div>
+							<div class="collapse navbar-collapse" id="navcol-1">
+								<ul class="nav navbar-nav navbar-left">
+									<li role="presentation"><a href="index.php">Home</a></li>
+									<li role="presentation"><a href="itinerary.php">Bus Listing</a></li>
+									<li role="presentation"><a href="team.php">Team</a></li>
+									<li role="presentation"><a class="active" href="contact.php">Contact Us</a></li>
+								</ul>
+								<ul class="nav navbar-nav navbar-right">
+									<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+									<li><a href="sign_up.php"><span class="fa fa-user-plus"></span> Sign Up</a></li>
+								</ul>
+							</div>
+
+						</div>
+					</nav>
+					<!<BR><BR><br>
 					<div class="container">
-						<div class="navbar-header"><a href="index.php" class="navbar-brand navbar-link"><i class="glyphicon glyphicon-phone"></i>TickIT</a>
-							<button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggle collapsed"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-						</div>
-						<div class="collapse navbar-collapse" id="navcol-1">
-							<ul class="nav navbar-nav navbar-left">
-								<li role="presentation"><a href="index.php">Home</a></li>
-								<li role="presentation"><a href="itinerary.php">Bus Listing</a></li>
-								<li role="presentation"><a href="team.php">Team</a></li>
-								<li role="presentation"><a class="active" href="contact.php">Contact Us</a></li>
-							</ul>
-							<ul class="nav navbar-nav navbar-right">
-								<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-								<li><a href="sign_up.php"><span class="fa fa-user-plus"></span> Sign Up</a></li>
-							</ul>
-						</div>
+		  <h1>THE TEAM</h1>
+		  
+		  <div class="row">
+		    <div class="col-sm-4" style="background-color:lavender;">
+		    	<div class="panel panel-default">
+		  			<div class="panel-heading">
+		    			<h3 class="panel-title">Brenda</h3>
+		  			</div>
+		  			<div class="panel-body">
+		   				<img class="img-responsive" src="brenda.jpg" alt="brenda Mboya" width="304" height="236">
+				                
+				                <p>Brenda is a design enthusiast.she designed the <a href="https://teamtickit.wordpress.com/">tickit website</a> and worked on the team and contact pages</p>
+		  			</div>
+				</div>
+				<div class="panel panel-default">
+		  			<div class="panel-heading">
+		    			<h3 class="panel-title">Vladmir Fomene
+		  			</div>
+		  			<div class="panel-body">
+		   				<img class="img-responsive" src="vlad.jpg " width="304 height="236">
+				                
+				                <p>Lead back-end engineer and database designer.He also worked on the page templates for the application</p>
+		  			</div>
+				</div>
+		    	
+		    
+		    </div>
+		    <div class="col-sm-4" style="background-color:lavender;">
+		    <div class="panel panel-default">
+		  			<div class="panel-heading">
+		    			<h3 class="panel-title">Linda Bhebhe
+		  			</div>
+		  			<div class="panel-body">
+		   				<img class="img-responsive" src="bhebhe.jpg " width="304 height="236">
+				                
+				                <p>Database implementer.She worked on the log in  and the sign up page</p>
+		  			</div>
+		    	
+		    </div>
+		    </div>
+		    <div class="col-sm-4" style="background-color:lavender;">
+		    <div class="panel panel-default">
+		  			<div class="panel-heading">
+		    			<h3 class="panel-title">Abdul Razak
+		  			</div>
+		  			<div class="panel-body">
+		   				<img class="img-responsive" src="razak.jpg " width="300" height="150">
+				                
+				                <p>Front end designer for the home page</p>
+		  			</div>
+		    	
+		    </div>
+		    
+		    <div class="col-sm-12" style="background-color:lavender;">
+		    <div class="panel panel-default">
+		  			<div class="panel-heading">
+		    			<h3 class="panel-title">Job Mwesigwa
+		  			</div>
+		  			<div class="panel-body">
+		   				<img class="img-responsive" src="Job.jpg " width="304" height="236">
+				                
+				                <p>Job worked on the bus listing page.</p>
+		  			</div>
+		    	
+		    </div>
+		    
+		    
+				
+		   
+		</div>
+	             
+				               
 
-					</div>
-				</nav>
-				<!<BR><BR>
-				<div class="container">
-	  <h1>THE TEAM</h1>
-	  
-	  <div class="row">
-	    <div class="col-sm-4" style="background-color:lavender;">
-	    	<div class="panel panel-default">
-	  			<div class="panel-heading">
-	    			<h3 class="panel-title">Brenda</h3>
-	  			</div>
-	  			<div class="panel-body">
-	   				<img class="img-responsive" src="brenda.jpg" alt="brenda Mboya" width="304" height="236">
-			                
-			                <p>Brenda is a design enthusiast.she designed the <a href="https://teamtickit.wordpress.com/">tickit website</a> and worked on the team and contact pages</p>
-	  			</div>
-			</div>
-			<div class="panel panel-default">
-	  			<div class="panel-heading">
-	    			<h3 class="panel-title">Vladmir Fomene
-	  			</div>
-	  			<div class="panel-body">
-	   				<img class="img-responsive" src="vlad.jpg " width="304 height="236">
-			                
-			                <p>Brenda is a design enthusiast.she designed the <a href="https://teamtickit.wordpress.com/">tickit website</a> and worked on the team and contact pages</p>
-	  			</div>
-			</div>
-	    	
-	    
-	    </div>
-	    <div class="col-sm-4" style="background-color:lavender;">
-	    	<img class="img-responsive" src="bhebhe.jpg" alt="brenda Mboya" width="304" height="200">
-			                <h3>Linda Bhebhe </h3>
-			                <p>Brenda is a design enthusiast.she designed the <a href="https://teamtickit.wordpress.com/">tickit website</a> and worked on the team and contact pages</p>
-	    </div>
-	    <div class="row" style="margin:50px; margin-bottom: 100px;">
-			<div class="col-md-2">
-				&nbsp
-			</div>
-	    <div class="col-sm-4" style="background-color:lavender;">
-	    	<img class="img-responsive" src="razak.jpg" alt="brenda Mboya" width="304" height="236">
-			                <h3>Abdul Razak</h3>
-			                <p>Brenda is a design enthusiast.she designed the <a href="https://teamtickit.wordpress.com/">tickit website</a> and worked on the team and contact pages</p>
-	    </div>
-	    <div class="row" style="margin:50px; margin-bottom: 100px;">
+						
+				
+
 			
-	    <div class="col-sm-4" style="background-color:lavender;">
-	    	<img class="img-responsive" src="Job.jpg" alt="brenda Mboya" width="304" height="236">
-			                <h3>Job Mwesigwa</h3>
-			                <p>Brenda is a design enthusiast.she designed the <a href="https://teamtickit.wordpress.com/">tickit website</a> and worked on the team and contact pages</p>
-	    </div>
-	  </div>
-	</div>
-			<!--<div class="grid">
-			        <div class="row">
-			            <div class="col-lg-12">
-			            <br><br>
-			                <h2 class="page-header" >Our Team</h2>
-			            </div>
-			        </div>
-			        <div class="row">
-			        <div class="col-md-4">
-			        <img class="img-responsive" src="brenda.jpg" alt="brenda Mboya" width="304" height="236">
-			                <h3>Brenda Mboya</h3>
-			                <p>Brenda is a design enthusiast.she designed the <a href="https://teamtickit.wordpress.com/">tickit website</a> and worked on the team and contact pages</p>
-
-			        </div>
-			        </div>
-
-			        <div class="col-md-4">
-			        <img class="img-responsive" src="brenda.jpg" alt="brenda Mboya" width="304" height="236">
-			                <h3>Brenda Mboya</h3>
-			                <p>Brenda is a design enthusiast.she designed the <a href="https://teamtickit.wordpress.com/">tickit website</a> and worked on the team and contact pages</p>
-
-			        </div>
-			        </div>
-			            -->
-			                
-			               
-
-					
-			
-
-			</body>
-			<!--the footer-->
-			<footer style="text-align: center; /*padding-top: 50%*/" id="footer">
-					<div class="container">
-						<div class="custom"  >
-							<p>©TickIT Company Ltd. All rights reserved.</p>
-							<p>1 University Avenue, PMB CT3321 | Cantonments, Accra, Ghana | Phone: +233.000.000.00</p>
-							<p><a href="index.php" rel="alternate">TickIT Company Ltd</a> | <a href="book_ticket" rel="alternate">Ticket Booking</a></p>
+				<!--the footer-->
+				<footer style="text-align: center; /*padding-top: 50%*/" id="footer">
+						<div class="container">
+							<div class="custom"  >
+								<p>©TickIT Company Ltd. All rights reserved.</p>
+								<p>1 University Avenue, PMB CT3321 | Cantonments, Accra, Ghana | Phone: +233.000.000.00</p>
+								<p><a href="index.php" rel="alternate">TickIT Company Ltd</a> | <a href="book_ticket" rel="alternate">Ticket Booking</a></p>
+							</div>
 						</div>
-					</div>
 
-				</footer>
-			</html>
+					</footer>
+					</body>
+				</html>

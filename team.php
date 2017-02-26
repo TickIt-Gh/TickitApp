2c8f4183328adf337d9a7468c397159d1bdf6eb4
Job mwesigwa <Job mwesigwa>
2017-02-26 23:56:36 +0000

    To be uploaded on the server

--- team.php
+++ team.php
@@ -1,13 +1,35 @@
-<?php
-	require_once 'require/header.php';
-?>
-	<title>Welcome to TickIT</title>
+				<html>
+				<head>
+					<title>teamPage</title>
+					<meta name="viewport" content="width=device-width, initial-scale=1">
+					<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+					<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+					<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+					<link rel="stylesheet" type="text/css" href="css/style.css">
+					<link rel="stylesheet" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+				</head>
+				<body>
+				<nav class="navbar navbar-default navbar-fixed-top" role='navigation'>
+						<div class="container">
+							<div class="navbar-header"><a href="index.php" class="navbar-brand navbar-link"><i class="glyphicon glyphicon-phone"></i>TickIT</a>
+								<button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggle collapsed"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+							</div>
+							<div class="collapse navbar-collapse" id="navcol-1">
+								<ul class="nav navbar-nav navbar-left">
+									<li role="presentation"><a href="index.php">Home</a></li>
+									<li role="presentation"><a href="itinerary.php">Bus Listing</a></li>
+									<li role="presentation"><a href="team.php">Team</a></li>
+									<li role="presentation"><a class="active" href="contact.php">Contact Us</a></li>
+								</ul>
+								<ul class="nav navbar-nav navbar-right">
+									<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+									<li><a href="sign_up.php"><span class="fa fa-user-plus"></span> Sign Up</a></li>
+								</ul>
+							</div>
 
-<?php
-	require_once 'require/nav_bar.php';
-?>
-
-					<BR><BR><br>
+						</div>
+					</nav>
+					<!<BR><BR><br>
 					<div class="container">
 		  <h1 class="text-center">THE TEAM</h1>
 		  
@@ -87,6 +109,18 @@
 
 			
 				<!--the footer-->
-				<?php
-					require_once 'require/footer.php';
-				?>
+				<footer style="text-align: center; /*padding-top: 50%*/" id="footer">
+						<div class="container">
+							<div class="custom"  >
+								<p>©TickIT Company Ltd. All rights reserved.</p>
+								<p>1 University Avenue, PMB CT3321 | Cantonments, Accra, Ghana | Phone: +233.000.000.00</p>
+								<p><a href="index.php" rel="alternate">TickIT Company Ltd</a> | <a href="book_ticket" rel="alternate">Ticket Booking</a></p>
+							</div>
+						</div>
+
+					</footer>
+
+				</body>
+				
+					
+				</html>

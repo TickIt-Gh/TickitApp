18192bfe84fad2aed2e8c69994ce1bae954465dc
Vladimir Fomene <vladimirfomene@gmail.com>
2017-04-14 16:39:56 +0000

    Adding getListing|editListing|UpdateListing

--- classes/admin.php
+++ classes/admin.php
@@ -8,6 +8,10 @@
 
 //Require our database connection class.
 require_once("../database/Database.php");
+
+//Require bus listing class
+require_once("listing.php");
+
 public class Admin{
   private $email;
 
@@ -71,15 +75,58 @@ public class Admin{
     $company = $newCompany;
   }
 
-  public function displayDashboard(){
+  public function getListings(){
+    $db = new Datbase;
+    $sql = "SELECT listing_id, bus_number, available_seats, depature_time,
+    departure_date, departure_point, destination_point, listing_status, price, managed_by
+    FROM bus_listing";
+    $result = $db->query($sql);
+
+    if(!$result){
+      print("Could not get records from the database.");
+      exit;
+    }
+    $records = $db->fetchAll();
+    return $records;
+  }
+
+  public function editListing($listingID){
+    $db = new Datbase;
+    $sql = "SELECT listing_id, bus_number, available_seats, depature_time,
+    departure_date, departure_point, destination_point, listing_status, price, managed_by
+    FROM bus_listing WHERE listing_id = $listingID";
+    $result = $db->query($sql);
+
+    if(!$result){
+      print("Could not get records from the database.");
+      exit;
+    }
+    return $db->fetch_json();
 
   }
 
-  public function editListing(){
+  public function UpdateListing($listing){
+    $db = new Datbase;
+    $sql = "UPDATE bus_listing SET listing_id = $listing->getListingId() ,
+    bus_number = $listing->getBusNumber() , available_seats = $listing->getAvailableSeats(),
+    depature_time = '$listing->getDepartureTime()', departure_date = '$listing->getDepartureDate()',
+    departure_point = '$listing->getDeparturePoint()' , destination_point = '$listing->getDestinationPoint()',
+    listing_status = '$listing->getListingStatus()', price = $listing->getPrice(), managed_by = $listing->getManagedBy()
+    WHERE listing_id = $listing->getListingId()";
 
+    return $db->query($sql);
   }
 
-  public function addListing(){
+  public function addListing($listing){
+    $db = new Datbase;
+    $sql = "INSERT INTO bus_listing (listing_id, bus_number, available_seats, depature_time,
+    departure_date, departure_point, destination_point, listing_status, price, managed_by)
+    VALUES ($listing->getListingId(), $listing->getBusNumber(), $listing->getAvailableSeats(),
+    '$listing->getDepartureTime()', '$listing->getDepartureDate()', '$listing->getDeparturePoint()',
+    '$listing->getDestinationPoint()', '$listing->getListingStatus()',
+    $listing->getPrice(), $listing->getManagedBy()";
+
+    return $db->query($sql);
 
   }
 }

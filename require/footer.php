c0ceb14ff98cc3a158e5a3c5303a184f67609f64
Vladimir Fomene <vladimirfomene@gmail.com>
2017-04-16 17:07:15 +0000

    Add js code to footer

--- require/footer.php
+++ require/footer.php
@@ -1,13 +1,14 @@
-<footer id="footer" style="text-align: center;">
-    <div class="container">
-        <div class="custom">
-            <p>©TickIT Company Ltd. All rights reserved.</p>
-            <p>1 University Avenue, PMB CT3321 | Cantonments, Accra, Ghana | Phone: +233.000.000.00</p>
-            <p><a href="../pages/index.php" rel="alternate">TickIT Company Ltd</a> | <a href="../pages/itinerary.php"
-                                                                                        rel="alternate">Ticket
-                    Booking</a></p>
-        </div>
-    </div>
-</footer>
+  <footer id="footer" style="text-align: center;">
+      <div class="container">
+          <div class="custom">
+              <p>©TickIT Company Ltd. All rights reserved.</p>
+              <p>1 University Avenue, PMB CT3321 | Cantonments, Accra, Ghana | Phone: +233.000.000.00</p>
+              <p><a href="../pages/index.php" rel="alternate">TickIT Company Ltd</a> | <a href="../pages/itinerary.php"
+                                                                                          rel="alternate">Ticket
+                      Booking</a></p>
+          </div>
+      </div>
+  </footer>
+  <script src="../js/script.js"></script>
 </body>
 </html>

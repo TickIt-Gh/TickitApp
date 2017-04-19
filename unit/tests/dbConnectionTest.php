2d9fb861473e960dc894b71ee2c6842a59be35d2
brenda.mboya <brendamboya.mboya@gmail.com>
2017-04-19 22:56:17 +0300

    tested the database connection using phpunit

--- unit/tests/dbConnectionTest.php
+++ unit/tests/dbConnectionTest.php
@@ -11,18 +11,7 @@ include_once("C:/xampp/htdocs/TickitApp/unit/database/Database.php");
 
 			$this->assertTrue($connect->connect());
 		}
-		// testing the real_escape method for preventing sql injection
-		/*public function testRealEscape(){
-
-			$connect = new dbconnection;
-			$sql="INSERT INTO useraccount (username,pwd,fname,lname,email,gender,major_id,userstatus,per_id)
-	VALUES('%s','%s','%s','%s','%s','%s','%s', '%s','%s')";
-			// $myList= array("Njoki","sizfaith","Lily","njoki","liliy@gmail.com","engineering","ACTIVE",1);
-
-			$this->assertTrue($connect->realEscape($sql,'Max','sizfaith','Lily','njoki','max@gmail.com', 'F','2','ACTIVE',1));
-
-
-		}**/
+		
 
 	}
 	?>

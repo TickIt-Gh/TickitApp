56a348f59e7562613bab7bf2f4ef59d523ae6175
razakadam74 <razakadam74@gmail.com>
2017-04-19 12:25:32 +0000

    redirection.php working redirectiong user to their respective layouts

--- setting/redirection.php
+++ setting/redirection.php
@@ -6,17 +6,27 @@
  * Time: 12:13 PM
  */
 
-if (isset($_SESSION)) {
-    redirect_user($_SESSION['is_admin']);
+if (isset($_SESSION)) { //check if session is set
+    if (isset($_SESSION['is_admin'])) {
+        redirect_user($_SESSION['is_admin']);
+    } else {
+        header('Location: ../public/login.php');
+    }
 } else {
     header('Location: ../public/login.php');
 }
 
+/**
+ * this function redirects user to their respective layouts
+ * @param $user_type user type
+ */
 function redirect_user($user_type)
 {
     if ($user_type === 'yes') {
         header('Location: ../pages/adminDashBoard.php');
     } elseif ($user_type === 'no') {
         header('Location: ../pages/standardUserBoard.php');
+    } else {
+        header('Location: ../public/login.php');
     }
 }

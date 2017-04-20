e48def1d8ac2e00e4965be3ef1cddaaf0d2dc25e
Bhebhe <linda.bhebhe@ashesi.edu.gh>
2017-04-20 11:27:40 +0100

    payment class

--- classes/payment.php
+++ classes/payment.php
@@ -5,6 +5,43 @@
 * @author Linda Bhebhe 
 */
 class Payment{
+
+	//properties 
+	private $listingID;
+	private $userID;
+	private $date;
+	private $method;
+
+
+
+    //methods
+
+	//constructor
+	public function __constructor(){
+
+	}
+
+
+public function getListingID(){
+	return $this->listingID;
+}
+
+public function getUserID(){
+		return $this->userID;
+}
+
+public function getDate(){
+		return $this->date;
+}
+
+public function getPaymentMethod(){
+		return $this->method;
+}
+
+public function insertPayment(){
+
+	
+}
 	
 
 }
---
+++ controller/contact_us_controller.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: razak
+ * Date: 4/20/2017
+ * Time: 11:10 AM
+ */
+
+

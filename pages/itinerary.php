95b022b88adc3bdaa9ac6e6c5b8387e9acbbc359
Vladimir Fomene <vladimirfomene@gmail.com>
2017-04-20 16:16:34 +0000

    Modify itinerary to pull bus_listing data from the database

--- pages/itinerary.php
+++ pages/itinerary.php
@@ -1,62 +1,12 @@
 <?php
-require_once '../setting/init.php';
-include_once REQUIRES . 'header.php';
-?>
-<title>Listing</title>
+  require_once '../setting/init.php';
+  require_once('../controller/bus_listingController.php');
 
-<?php
-include_once REQUIRES . 'nav_bar.php';
-?>
+  //Sets up the bus listing controller
+  $listingBoard = new busListingController;
 
-<div class="row dashboard" style="margin:50px; margin-bottom: 100px;">
-    <div class="col-md-2">
-        &nbsp
-    </div>
-    <div class="col-md-8"
-    ">
-    <div class="panel panel-default">
-        <!-- Default panel contents -->
-        <div class="panel-heading">Bus Listing</div>
+  //Displays the bus listing board for clients
+  $listingBoard->displayListingPage();
 
-        <!-- Table -->
-        <table class="table">
-            <tr>
-                <th>#</th>
-                <th>Bus Number</th>
-                <th>Departure Time</th>
-                <th>Departure Date</th>
-                <th>Departure Point</th>
-                <th>Destination Point</th>
-                <th>Action</th>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>GT 74 B</td>
-                <td>11:45am</td>
-                <td>26/02/2017</td>
-                <td>Accra</td>
-                <td>Kumasi</td>
-                <td><a class="btn btn-primary" href="buy.php">BUY</a></td>
-            </tr>
-            <tr>
-                <td>2</td>
-                <td>GT 547 C</td>
-                <td>11:45pm</td>
-                <td>27/02/2017</td>
-                <td>Tamale</td>
-                <td>Kumasi</td>
-                <td><a class="btn btn-primary" href="buy.php">BUY</a></td>
-            </tr>
-        </table>
 
-    </div>
-</div>
-<div class="col-md-2">
-    &nbsp
-</div>
-
-</div>
-
-<?php
-include_once REQUIRES . 'footer.php';
 ?>

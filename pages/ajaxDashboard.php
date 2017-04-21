79c56252fa5b0a5c24165f84f00ce940c976a0e3
Vladimir Fomene <vladimirfomene@gmail.com>
2017-04-21 16:23:24 +0000

    Ajax for adding listing

--- pages/ajaxDashboard.php
+++ pages/ajaxDashboard.php
@@ -13,6 +13,27 @@ if(isset($_GET['listingID']) && $_GET['listingID']){
 
 }
 
+if(isset($_GET['bus_number']) && isset($_GET['available_seats']) && isset($_GET['departure_time'])
+ && isset($_GET['departure_date']) && isset($_GET['departure_point'])
+ && isset($_GET['destination_point']) && isset($_GET['listing_status']) && isset($_GET['listing_price'])
+&& isset($_GET['add'])){
+  $listing = new Listing;
+  $listing->setBusNumber($_GET['bus_number']);
+  $listing->setAvailableSeats((int) $_GET['available_seats']);
+  $listing->setDepartureTime($_GET['departure_time']);
+  $listing->setDepartureDate($_GET['departure_date']);
+  $listing->setDeparturePoint($_GET['departure_point']);
+  $listing->setDestinationPoint($_GET['destination_point']);
+  $listing->setListingStatus($_GET['listing_status']);
+  $listing->setPrice((float) $_GET['listing_price']);
+  $listing->setManagedBy($admin->getAdminID());
+  if($admin->addListing($listing)){
+    $dashController->displaydashboard($admin);
+  }else{
+    echo "Ajax Failed";
+  }
+}
+
 
 
 

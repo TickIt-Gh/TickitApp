6f8d64c0b1a6eb5becf22edf0ece4ef579de28cb
razakadam74 <razakadam74@gmail.com>
2017-04-22 11:50:13 +0000

    standard user nav bar works

--- pages/standardUserBoard.php
+++ pages/standardUserBoard.php
@@ -6,8 +6,21 @@
  * Time: 12:17 PM
  */
 session_start();
+
 if (isset($_SESSION['userID'])) {
 
 } else {
     header('Location: ../public/login.php');
 }
+
+
+include_once '../setting/init.php';
+include_once REQUIRES . 'header.php';
+include_once LAYOUT . 'normal_user_layout.php';
+
+
+?>
+
+<?php
+include_once REQUIRES . 'footer.php';
+?>

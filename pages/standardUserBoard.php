fb954f3cd65b896a5c40a8d8ec71d72907656844
brenda.mboya <brendamboya.mboya@gmail.com>
2017-04-22 16:18:44 +0300

    the standard user page is up-to-date

--- pages/standardUserBoard.php
+++ pages/standardUserBoard.php
@@ -1,16 +1,25 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: razak
+ * User: razak and Brenda Mboya
  * Date: 4/19/2017
  * Time: 12:17 PM
  */
 session_start();
 
 if (isset($_SESSION['userID'])) {
+require_once '../setting/init.php';
+  require_once('../controller/buyController.php');
+  require_once('../controller/bus_listingController.php');  
+  require_once LAYOUT . 'admin_layout.php';
+
+  //Sets up the bus listing controller
+  $listingBoard = new busListingController;
+
+  //Displays the bus listing board for clients
+  $listingBoard->displayListingPage();
 
 
-header('Location: ../pages/itinerary.php');
 
 } else {
     header('Location: ../public/login.php');

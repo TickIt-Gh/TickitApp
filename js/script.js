9ab651bd957652ffed8243024f8d087f310723c1
Job mwesigwa <Job mwesigwa>
2017-04-23 21:51:52 +0000

    THe tocken up for server

--- js/script.js
+++ js/script.js
@@ -335,7 +335,11 @@ function validate_buy_form() {
     if (session == 1)
     {
         document.getElementById('pop').innerHTML = '<div id="editModal" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel"><div class="modal-dialog modal-lg" role="document"><div class="modal-content"><h1 class="text-center">Thank You For Using <br> <br> TickIT</h1><h3 class="text-center"><em>Your Tocken is <strong id="tocken"></strong></em></h3><br> <h5 class="text-center"><em>It has been sent to your email</em></h5><br> <button type="button" class="btn btn-default center-block" data-dismiss="modal">Close</button></div> </div> </div>';
-        document.getElementById("tocken").innerHTML = tocken();
+        var mytocken= tocken();
+        document.getElementById("tocken").innerHTML = mytocken;
+
+        //sending tocken to server side
+       window.location.href = "../controller/buyController.php?tocken=" + mytocken; 
     }
 
     //user not logged in

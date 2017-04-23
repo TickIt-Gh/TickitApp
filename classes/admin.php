2b93092f17bfc70d0f00ee59cc5026ca414c7c43
razakadam74 <razakadam74@gmail.com>
2017-04-23 20:40:38 +0000

    Admin class commented

--- classes/admin.php
+++ classes/admin.php
@@ -1,10 +1,10 @@
 <?php
 /**
-*@author VladimirFomene
-*Admin class with all attributes and functionalities of an
-* admin class.
-*
-**/
+ * @author VladimirFomene
+ *Admin class with all attributes and functionalities of an
+ * admin class.
+ *
+ **/
 
 //Require our database connection class.
 require_once("../database/Database.php");
@@ -14,155 +14,243 @@ require_once("../database/Database.php");
 require_once("listing.php");
 require_once("User.php");
 
-class Admin extends User{
-  private $email;
+class Admin extends User
+{
+    private $email;
+    private $adminID;
+    private $password;
+    private $session;
+    private $status = "inactive";
+    private $role;
+    private $company;
 
-  private $adminID;
+    /**
+     * constructor of admin
+     */
+    public function __constructor()
+    {
 
-  private $password;
+    }
 
-  private $session;
+    /**
+     * gets the admin id
+     * @return mixed ID of admin
+     */
+    public function getAdminID()
+    {
+        return $this->adminID;
+    }
 
-  private $status = "inactive";
+    /**
+     * sets a new id of admin
+     * @param $newID new id
+     */
+    public function setAdminID($newID)
+    {
+        $this->adminID = $newID;
+    }
 
-  private $role;
+    /**
+     * this gets the session of the admin
+     * @return mixed  session of user
+     */
+    public function getSession()
+    {
+        return $this->session;
+    }
 
-  private $company;
+    /**
+     * set the admin new session
+     * @param $newSession new of session
+     */
+    public function setSession($newSession)
+    {
+        $this->session = $newSession;
+    }
 
-  public function __constructor(){
+    /**
+     * gets admin password
+     * @return mixed password of admin
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
 
-  }
+    /**
+     * set password
+     * @param $newPass new password
+     */
+    public function setPassword($newPass)
+    {
+        $this->password = $newPass;
+    }
 
-  public function getAdminID(){
-    return $this->adminID;
-  }
+    /**
+     * get admin email
+     * @return mixed email of admin
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
 
-  public function setAdminID($newID){
-    $this->adminID = $newID;
-  }
+    /**
+     * set admin email
+     * @param $newEmail new aemail
+     */
+    public function setEmail($newEmail)
+    {
+        $this->email = $newEmail;
+    }
 
-  public function getSession(){
-    return $this->session;
-  }
+    /**
+     * get role of admin
+     * @return mixed role of admin
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
 
-  public function setSession($newSession){
-    $this->session = $newSession;
-  }
+    /**
+     * set role of admin
+     * @param $newRole new role
+     */
+    public function setRole($newRole)
+    {
+        $this->role = $newRole;
+    }
 
-  public function getPassword(){
-    return $this->password;
-  }
+    /**
+     * get comapany of admin
+     * @return mixed company of admin
+     */
+    public function getCompany()
+    {
+        return $this->company;
+    }
 
-  public function setPassword($newPass){
-    $this->password = $newPass;
-  }
+    /**
+     * set company
+     * @param $newCompany new company
+     */
+    public function setCompany($newCompany)
+    {
+        $this->company = $newCompany;
+    }
 
-  public function getEmail(){
-    return $this->email;
-  }
-
-  public function setEmail($newEmail){
-    $this->email = $newEmail;
-  }
-
-  public function getRole(){
-    return $this->role;
-  }
-
-  public function setRole($newRole){
-    $this->role = $newRole;
-  }
-
-  public function getCompany(){
-    return $this->company;
-  }
-
-  public function setCompany($newCompany){
-    $this->company = $newCompany;
-  }
-
-  public function getListings(){
-    $db = new Database;
-    $sql = "SELECT listing_id, bus_number, available_seats, depature_time,
+    /**
+     * get bus listing
+     * @return array|bool|null array of all bus listing
+     */
+    public function getListings()
+    {
+        $db = new Database;
+        $sql = "SELECT listing_id, bus_number, available_seats, depature_time,
     departure_date, departure_point, destination_point, listing_status, price, managed_by
     FROM bus_listing";
-    $result = $db->query($sql);
+        $result = $db->query($sql);
 
-    if(!$result){
-      print("Could not get records from the database.");
-      exit;
+        if (!$result) {
+            print("Could not get records from the database.");
+            exit;
+        }
+        $records = $db->fetchAll();
+        return $records;
     }
-    $records = $db->fetchAll();
-    return $records;
-  }
 
-  public function editListing($listingID){
-    $db = new Database;
-    $sql = "SELECT listing_id, bus_number, available_seats, depature_time,
+    /**
+     * this function get the info of a bus to edit
+     * @param $listingID listingId of a bus
+     * @return bool|string info of a listing
+     */
+    public function editListing($listingID)
+    {
+        $db = new Database;
+        $sql = "SELECT listing_id, bus_number, available_seats, depature_time,
     departure_date, departure_point, destination_point, listing_status, price, managed_by
     FROM bus_listing WHERE listing_id = $listingID";
-    $result = $db->query($sql);
+        $result = $db->query($sql);
 
-    if(!$result){
-      print("Could not get records from the database.");
-      exit;
+        if (!$result) {
+            print("Could not get records from the database.");
+            exit;
+        }
+        return $db->fetch_json();
+
     }
-    return $db->fetch_json();
 
-  }
-
-  public function UpdateListing($listing){
-    $db = new Database;
-    $listingID = $listing->getListingId();
-    $busNumber = $listing->getBusNumber();
-    $availableSeats = $listing->getAvailableSeats();
-    $departureTime = $listing->getDepartureTime();
-    $departureDate = $listing->getDepartureDate();
-    $departurePoint = $listing->getDeparturePoint();
-    $destination = $listing->getDestinationPoint();
-    $status = $listing->getListingStatus();
-    $price = $listing->getPrice();
-    $manager = $listing->getManagedBy();
-    $sql = "UPDATE bus_listing SET listing_id = $listingID,
+    /**
+     * update the info a bus
+     * @param $listing listing information json
+     * @return return  true if successful
+     */
+    public function UpdateListing($listing)
+    {
+        $db = new Database;
+        $listingID = $listing->getListingId();
+        $busNumber = $listing->getBusNumber();
+        $availableSeats = $listing->getAvailableSeats();
+        $departureTime = $listing->getDepartureTime();
+        $departureDate = $listing->getDepartureDate();
+        $departurePoint = $listing->getDeparturePoint();
+        $destination = $listing->getDestinationPoint();
+        $status = $listing->getListingStatus();
+        $price = $listing->getPrice();
+        $manager = $listing->getManagedBy();
+        $sql = "UPDATE bus_listing SET listing_id = $listingID,
     bus_number = '$busNumber' , available_seats = $availableSeats,
     depature_time = '$departureTime', departure_date = '$departureDate ',
     departure_point = '$departurePoint' , destination_point = '$destination',
     listing_status = '$status', price = $price, managed_by = $manager
     WHERE listing_id = $listingID";
 
-    return $db->query($sql);
-  }
+        return $db->query($sql);
+    }
 
-  public function deleteListing($listingID){
-    $db = new Database;
-    $sql = "DELETE FROM bus_listing
+    /**
+     * delete a listing
+     * @param $listingID listing id
+     * @return return true of successful
+     */
+    public function deleteListing($listingID)
+    {
+        $db = new Database;
+        $sql = "DELETE FROM bus_listing
         WHERE listing_id = $listingID";
 
-    return $db->query($sql);
-  }
+        return $db->query($sql);
+    }
 
-  public function addListing($listing){
-    $db = new Database;
-    $busNumber = $listing->getBusNumber();
-    $availableSeats = $listing->getAvailableSeats();
-    $departureTime = $listing->getDepartureTime();
-    $departureDate = $listing->getDepartureDate();
-    $departurePoint = $listing->getDeparturePoint();
-    $destination = $listing->getDestinationPoint();
-    $status = $listing->getListingStatus();
-    $price = $listing->getPrice();
-    $manager = $listing->getManagedBy();
+    /**
+     * add a bus
+     * @param $listing info of listing
+     * @return return true if successful
+     */
+    public function addListing($listing)
+    {
+        $db = new Database;
+        $busNumber = $listing->getBusNumber();
+        $availableSeats = $listing->getAvailableSeats();
+        $departureTime = $listing->getDepartureTime();
+        $departureDate = $listing->getDepartureDate();
+        $departurePoint = $listing->getDeparturePoint();
+        $destination = $listing->getDestinationPoint();
+        $status = $listing->getListingStatus();
+        $price = $listing->getPrice();
+        $manager = $listing->getManagedBy();
 
-    $sql = "INSERT INTO bus_listing (listing_id, bus_number, available_seats, depature_time,
+        $sql = "INSERT INTO bus_listing (listing_id, bus_number, available_seats, depature_time,
     departure_date, departure_point, destination_point, listing_status, price, managed_by)
     VALUES (NULL, '$busNumber', $availableSeats,
     '$departureTime', '$departureDate', '$departurePoint',
     '$destination', '$status',
     $price, $manager)";
 
-    return $db->query($sql);
+        return $db->query($sql);
 
-  }
+    }
 }
 
 ?>
